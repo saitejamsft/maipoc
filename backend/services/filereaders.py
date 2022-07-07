b84b381c78d80b3b93e98574adce30db05b8d4c2
sai <sai@msft.com>
2022-07-07 14:09:39 +0530

    caching modified

--- backend/services/filereaders.py
+++ backend/services/filereaders.py
@@ -1,17 +1,25 @@
+import logging
 import pandas as pd
 
 
 cache_files = {}
-removable_files = ["opty/Feature_Contribution.csv", "opty/model_features.csv", "opty/Oppty_Amendment.csv", "opty/Resource_Resource_Tier_Mapping.csv", "opty/soft_exception.csv", "risk_recommender/Signed_Completed_Deals_FY20FY21FY22_C1_YAT_risks.csv", "risk_recommender/Open_Deals_Resource_Grain_FY22.csv"]
+removable_files = ["opty/Feature_Contribution.csv", "opty/model_features.csv", "opty/Oppty_Amendment.csv", "opty/Resource_Resource_Tier_Mapping.csv",
+                   "opty/soft_exception.csv", "risk_recommender/Signed_Completed_Deals_FY20FY21FY22_C1_YAT_risks.csv", "risk_recommender/Open_Deals_Resource_Grain_FY22.csv"]
+
 
 def get_data(file_name, file_type, file_encoding=None):
     if file_name in cache_files:
         return cache_files[file_name]
     else:
-        # print(f"read again {file_name}")
+        logging.info(f"read again {file_name}")
         if len(cache_files) > 10:
-            for name in removable_files:
-                cache_files.pop(f"local_files/{name}", None)
+            all_keys = list(cache_files.keys())
+            for name in all_keys:
+                if not "opty" in name:
+                    cache_files.pop(f"{name}", None)
+
+            # for name in removable_files:
+            #     cache_files.pop(f"local_files/{name}", None)
         file_path = (file_name)
         if file_type == 'csv':
             if file_encoding:
